8108f3d66fe1654c133467703066763c36846946
iqwertz <juliushussl@gmail.com>
2022-11-05 23:59:02 +0100

    added preview image renderer

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+chmod +x ./bin/svg2gcode
+mkdir -p output
+./bin/svg2gcode  input/image.svg --output output/gcode.nc --image output/
+cp input/image.svg output/image.svg
+mv output/image.png output/preview.png
